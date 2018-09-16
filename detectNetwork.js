99f2be720846899eea1685ca1ccf252b53858cad
Johnson Kwok <johnsonkwok1@gmail.com>
2018-09-15 19:29:36 -0700

    Add outline in comments for detectNetwork function

--- detectNetwork.js
+++ detectNetwork.js
@@ -11,8 +11,18 @@ var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
-
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+
+  // My outline
+  // input: a credit card number as a STRING
+  // output: name of the network as a string
+  // assumptions: the card number given will always be a string; we only have two networks in this scenario
+  // create an output variable (network) that is a string set to 'Invalid Number'
+  // get the prefix of the card number as a string
+  // if the prefixes and card number length match the format of a specific network
+  	// set output variable (network) to the appropriate network
+ 	// return network variable
+
 };
 
 
