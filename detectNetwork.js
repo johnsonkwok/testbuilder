12fcb89ef39cacd3afcf0926413b19a190625faa
Johnson Kwok <johnsonkwok1@gmail.com>
2018-09-16 16:41:28 -0700

    Add functionality for China UnionPay network

--- detectNetwork.js
+++ detectNetwork.js
@@ -2,16 +2,7 @@
 // name of a network, like 'MasterCard' or 'American Express'
 // Example: detectNetwork('343456789012345') should return 'American Express'
 
-// How can you tell one card network from another? Easy! 
-// There are two indicators:
-//   1. The first few numbers (called the prefix)
-//   2. The number of digits in the number (called the length)
-
 var detectNetwork = function(cardNumber) {
-  // Note: `cardNumber` will always be a string
-  // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
-  // The American Express network always starts with a 34 or 37 and is 15 digits long
- 
   // My outline
   // input: a credit card number as a STRING
   // output: name of the network as a string
@@ -28,26 +19,29 @@ var detectNetwork = function(cardNumber) {
 	const prefix2 = parseInt(cardNumber.slice(0, 2));
 	const prefix3 = parseInt(cardNumber.slice(0, 3));
 	const prefix4 = parseInt(cardNumber.slice(0, 4));
+	const prefix6 = parseInt(cardNumber.slice(0, 6));
  	const length = cardNumber.length;
- 	if ((prefix2 === 38 || prefix2 === 39) && length === 14) {
- 		network = `Diner's Club`;
- 	} else if ((prefix2 === 34 || prefix2 === 37) && length === 15) {
- 		network = 'American Express';
- 	} else if (prefix2 >= 51 && prefix2 <= 55 && length === 16) {
- 		network = 'MasterCard';
- 	} else if (prefix1 === 4 && (length === 13 || length === 16 || length === 19)) {
- 		network = 'Visa';
- 	} else if (isDiscover(prefix2, prefix3, prefix4, length)) {
-		network = 'Discover';
+	if (isChinaUnionPay(prefix3, prefix4, prefix6, length)) {
+		network = 'China UnionPay';
 	} else if (isMaestro(prefix4, length)) {
 		network = 'Maestro';
+	} else if (isDiscover(prefix2, prefix3, prefix4, length)) {
+		network = 'Discover';
+	} else if (prefix2 >= 51 && prefix2 <= 55 && length === 16) {
+		network = 'MasterCard';
+	} else if ((prefix2 === 34 || prefix2 === 37) && length === 15) {
+		network = 'American Express';
+	} else if ((prefix2 === 38 || prefix2 === 39) && length === 14) {
+ 		network = `Diner's Club`;
+ 	} else if (prefix1 === 4 && (length === 13 || length === 16 || length === 19)) {
+ 		network = 'Visa';
 	}
  	return network;
 };
 
-function isDiscover(pf2, pf3, pf4, length) {
-	if (pf2 === 65 || (pf3 >= 644 && pf3 <= 649) || pf4 === 6011) {
-		if (length === 16 || length === 19) {
+function isChinaUnionPay(pf3, pf4, pf6, length) {
+	if ((pf3 >= 624 && pf3 <= 626) || (pf4 >= 6282 && pf4 <= 6288) || (pf6 >= 622126 && pf6 <= 622925)) {
+		if (length >= 16 && length <= 19) {
 			return true;
 		}
 	}
@@ -57,6 +51,15 @@ function isDiscover(pf2, pf3, pf4, length) {
 function isMaestro(pf4, length) {
 	if (pf4 === 5018 || pf4 === 5020 || pf4 === 5038 || pf4 === 6304) {
 		if (length >= 12 && length <= 19) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function isDiscover(pf2, pf3, pf4, length) {
+	if (pf2 === 65 || (pf3 >= 644 && pf3 <= 649) || pf4 === 6011) {
+		if (length === 16 || length === 19) {
 			return true;
 		}
 	}
